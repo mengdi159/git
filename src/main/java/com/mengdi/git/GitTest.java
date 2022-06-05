794a27732ad48d36c8fd0d528627b636c6081536
mengdi <1596825608@qq.com>
2022-06-05 20:34:36 +0800

    first commit

---
+++ src/main/java/com/mengdi/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.mengdi.git;
+
+/**
+ * @author 孟迪
+ * @pseudonym 孟村一朵花
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
